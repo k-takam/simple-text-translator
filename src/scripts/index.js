017bbf0eec530bfb65c38bd5f9733cd7288d6ad9
k-takam <kazuya.takamatsu@gmail.com>
2016-04-01 22:41:23 +0900

    first commit

---
+++ src/scripts/index.js
@@ -0,0 +1,9 @@
+import css from '!style!css!sass!./../styles/style.sass';
+import React from 'react';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+render(
+  document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const webpack = require('webpack');
+const saveLicense = require('uglify-save-license');
+
+module.exports = {
+  cache: true,
+  entry: './src/scripts/index.js',
+  output: {
+    path: __dirname + '/dist/',
+    publicPath: "/dist/",
+    filename: 'bundle.js'
+  },
+  devtool: 'inline-source-map',
+  resolve: {
+    root: [path.join(__dirname, 'node_modules')],
+    extensions: ['', '.js', '.jsx', '.css', '.scss', '.sass']
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      output: {
+        comments: saveLicense
+      }
+    })
+  ],
+  module: {
+    preLoaders: [
+      {
+        test: /\.js|\.jsx$/,
+        loader: 'eslint',
+        exclude: /node_modules/
+      }
+    ],
+    loaders: [
+      {
+        test: /\.sass$/,
+        loaders: ['style', 'css', 'sass']
+      },
+      {
+        test: /\.js|\.jsx$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: { presets: ['es2015', 'react'] }
+      }
+    ]
+  },
+  eslint: { configFile: '.eslintrc' }
+};
